23799bb3cdc114d368cea045d9507700b1a82627
nihadkeric <nihadkeric@gmail.com>
2017-05-02 16:57:53 +0200

    add comunication with firebase in the recipe service

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,9 +1,17 @@
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../model/ingredient.model';
+import { Injectable, EventEmitter} from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
+
+@Injectable()
 export class RecipeService {
 
-  constructor() { }
+  recipeChanged = new EventEmitter<Recipe[]>();
+
+
+  constructor(private _http: Http) { }
 
   private recipes: Recipe[] = [
     new Recipe('Stack', 'Very nice', 'http://www.proveeduriapiaf.com.ar/epiaf/images/stories/virtuemart/product/roas-beff.jpg', [
@@ -32,6 +40,25 @@ export class RecipeService {
 
   editRecipe(oldRecipe: Recipe, newRecipe: Recipe){
     this.recipes[this.recipes.indexOf(oldRecipe)] = newRecipe;
+  }
+
+  storeData(){
+    const body = JSON.stringify(this.recipes);
+    const header = new Headers({
+      'Content-Type': 'application/json'
+    });
+    return this._http.put('https://ng2-recipe-http.firebaseio.com/recipe.json', body, {headers: header});
+  }
+
+  fetchData(){
+    this._http.get('https://ng2-recipe-http.firebaseio.com/recipe.json')
+    .map((response: Response) => response.json())
+    .subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+        this.recipeChanged.emit(this.recipes);
+      }
+    );
   }
 
 }
